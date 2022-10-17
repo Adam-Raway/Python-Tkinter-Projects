14178c267bcce2abb478dd95d6a0cfb44601cb00
Adam903288 <903288@pdsb.net>
2022-10-17 12:47:43 -0400

    Added query and delete functionality
    - User can now display all the names and addresses in the addresses table. Record OID is displayed alongside each record.
    - User can now delete individual records by entering the record's OID then pressing the delete button.

--- Databases/Databases.py
+++ Databases/Databases.py
@@ -8,7 +8,7 @@ from pyparsing import col
 
 root = Tk()
 root.title('Learning to use sqlite')
-root.geometry("400x400")
+# root.geometry("400x400")
 
 
 def submit():
@@ -16,6 +16,18 @@ def submit():
     conn = sqlite3.connect('Databases/address_book.db')
     # Creating a cursor instance
     c = conn.cursor()
+
+    '''
+    # Create table
+    c.execute("""CREATE TABLE addresses(
+        first_name text,
+        last_name text,
+        address text,
+        city text,
+        province text,
+        zipcode integer
+    )""")
+    '''
 
     # Insert into table
     c.execute("INSERT INTO addresses VALUES (:f_name, :l_name, :address, :city, :province, :zipcode)",
@@ -42,20 +54,46 @@ def submit():
     zipcode.delete(0, END)
 
 
-'''
-# Create table
-c.execute("""CREATE TABLE addresses(
-    first_name text,
-    last_name text,
-    address text,
-    city text,
-    province text,
-    zipcode integer
-)""")
-'''
+def query():
+    # Create a database or connect to one
+    conn = sqlite3.connect('Databases/address_book.db')
+    # Creating a cursor instance
+    c = conn.cursor()
+
+    # Query the database
+    c.execute("SELECT *, oid FROM addresses")
+    records = c.fetchall()
+
+    print_records = ""
+    for record in records:
+        print_records += f"{record[0]} {record[1]}: {record[2]} \t {record[6]}\n"
+
+    query_label = Label(root, text=print_records)
+    query_label.grid(row=9, column=0, columnspan=2)
+
+    # Commiting changes to a database
+    conn.commit()
+    # Close a connection
+    conn.close()
+
+
+def delete():
+    # Create a database or connect to one
+    conn = sqlite3.connect('Databases/address_book.db')
+    # Creating a cursor instance
+    c = conn.cursor()
+
+    c.execute(f"DELETE from addresses WHERE oid={delete_oid.get()}")
+
+    # Commiting changes to a database
+    conn.commit()
+    # Close a connection
+    conn.close()
+
+
 # Create Text Boxes
 f_name = Entry(root, width=30)
-f_name.grid(row=0, column=1, padx=20)
+f_name.grid(row=0, column=1, padx=20, pady=(10, 0))
 
 l_name = Entry(root, width=30)
 l_name.grid(row=1, column=1, padx=20)
@@ -75,7 +113,7 @@ zipcode.grid(row=5, column=1, padx=20)
 
 # Create Text Box Labels
 f_name_label = Label(root, text="First Name")
-f_name_label.grid(row=0, column=0)
+f_name_label.grid(row=0, column=0, pady=(10, 0))
 
 l_name_label = Label(root, text="Last Name")
 l_name_label.grid(row=1, column=0)
@@ -96,5 +134,16 @@ zipcode_label.grid(row=5, column=0)
 # Create submit button
 submit_btn = Button(root, text="Add Record to Database", command=submit)
 submit_btn.grid(row=6, column=0, columnspan=2, pady=10, padx=10, ipadx=100)
+
+# Create a query button
+query_btn = Button(root, text="Show Records", command=query)
+query_btn.grid(row=7, column=0, columnspan=2, pady=10, padx=10, ipadx=126)
+
+# Create a delete button
+delete_btn = Button(root, text="Delete Records", command=delete)
+delete_btn.grid(row=8, column=0, columnspan=2,
+                pady=10, padx=10, ipadx=70, sticky=W)
+delete_oid = Entry(root, width=10)
+delete_oid.grid(row=8, column=1, padx=(20, 10), sticky=E)
 
 root.mainloop()
