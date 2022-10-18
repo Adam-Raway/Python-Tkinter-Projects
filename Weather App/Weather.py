d9d5edd5d572932193494af279514280c9cfae1e
Adam903288 <903288@pdsb.net>
2022-10-18 14:38:11 -0400

    Created the Weather App folder
    - Created Weather App folder
    - Created Weather.py script with basic Tkinter code.
    - Imported a .ico file of some clouds.

---
+++ Weather App/Weather.py
@@ -0,0 +1,9 @@
+from tkinter import *
+from PIL import ImageTk, Image
+
+root = Tk()
+root.title('Weather App')
+root.iconbitmap('Weather App/Pictures/Clouds.ico')
+root.geometry('400x400')
+
+root.mainloop()
