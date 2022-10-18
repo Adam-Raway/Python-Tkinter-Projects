44c3b89711b59324956a43c016667df7cd2c20d2
Adam903288 <903288@pdsb.net>
2022-10-18 19:46:50 -0400

    Created basic functionality for Weather.py
    - Script connects to the API of the AirNow.gov site and loads the resultant .json file.
    - Reads the .json file to determine the air quality of a pre-determined zip-code then displays that in a window.
    - Window changes color to match the category name.

--- Weather App/Weather.py
+++ Weather App/Weather.py
@@ -1,9 +1,41 @@
 from tkinter import *
+from unicodedata import category
 from PIL import ImageTk, Image
+import requests
+import json
 
 root = Tk()
 root.title('Weather App')
 root.iconbitmap('Weather App/Pictures/Clouds.ico')
-root.geometry('400x400')
+root.geometry('400x40')
+
+try:
+    apiRequest = requests.get(
+        'https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=89129&distance=5&API_KEY=44E50F75-AC9C-4423-B7EF-813B4DBCF67F')
+    api = json.loads(apiRequest.content)
+
+    city = api[0]['ReportingArea']
+    aqiNum = api[0]['AQI']
+    aqiCategoryName = api[0]['Category']['Name']
+
+    if aqiCategoryName == "Good":
+        weather_color = "green"
+    elif aqiCategoryName == "Moderate":
+        weather_color = "yellow"
+    elif aqiCategoryName == "Unhealthy for Sensitive Groups":
+        weather_color = "orange"
+    elif aqiCategoryName == "Unhealthy":
+        weather_color = "red"
+    elif aqiCategoryName == 'Very Unhealthy':
+        weather_color = 'purple'
+    elif aqiCategoryName == 'Hazardous':
+        weather_color = 'black'
+
+    root.configure(bg=weather_color)
+    myLabel = Label(
+        root, text=f"{city}: {aqiNum}-{aqiCategoryName}", font=('Helvetica', 20), fg='White', bg=weather_color)
+    myLabel.pack()
+except Exception as e:
+    api = "Error"
 
 root.mainloop()
